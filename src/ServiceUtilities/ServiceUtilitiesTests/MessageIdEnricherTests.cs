729e81b394005b705437bf8d68aab9a6b3e18fc4
Markus Gartmann <markus.gartmann@mcs.ch>
2024-12-16 14:49:59 +0100

    initial logic

---
+++ src/ServiceUtilities/ServiceUtilitiesTests/MessageIdEnricherTests.cs
@@ -0,0 +1,27 @@
+﻿using mcs.ServiceUtilities.Logging;
+using mcs.ServiceUtilitiesTests.Mocks;
+using Moq;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Parsing;
+
+namespace mcs.ServiceUtilitiesTests
+{
+    public class MessageIdEnricherTests
+    {
+        [Fact]
+        public void Enrich_LogTwoEntries_ConsecutiveNumbering()
+        {
+            var logEvents = new LogEvent(new DateTimeOffset(), LogEventLevel.Information, null, new MessageTemplate("test", new List<MessageTemplateToken>()), new List<LogEventProperty>());
+            var propertyFactoryMock = new Mock<ILogEventPropertyFactory>();
+            propertyFactoryMock.Setup(x => x.CreateProperty(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>()))
+                .Returns((string name, int value, bool _) => new LogEventProperty(name, new LogEventPropertyValueMock(value)));
+            var enricher = new MessageIdEnricher();
+            enricher.Enrich(logEvents, propertyFactoryMock.Object);
+
+            Assert.Equal("1", logEvents.Properties["MessageId"].ToString());
+            enricher.Enrich(logEvents, propertyFactoryMock.Object);
+            Assert.Equal("2", logEvents.Properties["MessageId"].ToString());
+        }
+    }
+}
---
+++ src/ServiceUtilities/ServiceUtilitiesTests/Mocks/LogEventPropertyValueMock.cs
@@ -0,0 +1,20 @@
+﻿using Serilog.Events;
+
+namespace mcs.ServiceUtilitiesTests.Mocks
+{
+    internal class LogEventPropertyValueMock : LogEventPropertyValue
+    {
+        public LogEventPropertyValueMock(int value)
+        {
+            m_value = value;
+        }
+
+        public override void Render(TextWriter output, string? format = null, IFormatProvider? formatProvider = null)
+        {
+            output.Write(m_value);
+            return;
+        }
+
+        private int m_value = 0;
+    }
+}
